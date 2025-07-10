e7795325a51259878b36b0a9fa1c95bb7610f958
Joker-of-Gotham <1211095767@qq.com>
2025-07-10 15:43:45 +0800

    add new plugins and beautify the result

---
+++ _plugins/generate_collection_tags.rb
@@ -0,0 +1,36 @@
+# _plugins/generate_collection_tags.rb
+
+module Jekyll
+  class CollectionTagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      collections = ['graph_theory', 'posts'] # 自定义集合和_posts都加上
+      collections.each do |coll|
+        next unless site.collections[coll]
+
+        tags = site.collections[coll].docs.flat_map { |doc| doc.data['tags'] || [] }.uniq
+        tags.each do |tag|
+          site.pages << CollectionTagPage.new(site, site.source, coll, tag)
+        end
+      end
+    end
+  end
+
+  class CollectionTagPage < Page
+    def initialize(site, base, collection, tag)
+      @site = site
+      @base = base
+      @dir = File.join('tags', tag.to_s.downcase)
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['title'] = tag
+      self.data['collection'] = collection
+      self.data['tag'] = tag
+      self.data['layout'] = 'tag'
+      self.data['permalink'] = File.join('/tags', tag.to_s.downcase, '/')
+    end
+  end
+end
